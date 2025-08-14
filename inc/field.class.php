b54e804ff74a68bf33d270a4ef567cdee74f5d8b
akstis-typer <nikola22sat@mail.ru>
2025-08-14 16:28:09 +0300

    Fixed some bugs

--- inc/field.class.php
+++ inc/field.class.php
@@ -914,10 +914,10 @@ class PluginFieldsField extends CommonDBChild
     {
         $item = $params['item'];
 
-        if ($item->fields['type'] == "") {
+        if (isset($item->fields['type']) && $item->fields['type'] == "") {
             $item->fields['type'] = $params['options']['type'];
         }
-        if ($item->fields['itilcategories_id'] == "") {
+        if (isset($item->fields['itilcategories_id']) &&$item->fields['itilcategories_id'] == "") {
             $item->fields['itilcategories_id'] = $params['options']['itilcategories_id'];
         }
         $functions = array_column(debug_backtrace(), 'function');
